00bfee734b2bbaeef6b6effc98456ee7da93da46
unknown <tmartinez114@gmail.com>
2019-06-02 18:33:13 -0400

    completed express tutorial // added static page, queries,  routes, and params

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use('/', express.static('public'));
+
 app.get('/', (req, res) => res.send('Hello world'));
 
-app.get('/api', (req, res) => res.send('API route working'));
+app.get('/api', function (req, res) {
+    const userName = req.query.username;
+    const userId = req.query.id;
+    const message = req.query.message;
+    console.log(userName);
+    const reply = `${userName} with id of ${userId} is saying ${message}`
+    res.send(reply);
+})
+
+app.get('/showprofile/:username', function (req, res) {
+    const user = req.params.username;
+    console.log(user);
+
+    res.send('show profile working');
+})
+
 
 app.listen(port, () => console.log(`App is listening on port ${port}!`));
 
