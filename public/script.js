f95bfab7b5aadddcef5a9ab1dee026adaa088810
unknown <tmartinez114@gmail.com>
2019-06-03 13:37:59 -0400

    finished API calls lesson

--- public/script.js
+++ public/script.js
@@ -6,3 +6,27 @@ function getUserInfo() {
     document.getElementById('userInfo').innerHTML = response.data
     })
     }
+
+function handleSubmit() {
+    const userName = document.getElementById('user-id').value;
+    const userId = document.getElementById('user-id').value;
+    const message = document.getElementById('message').value;
+    console.log(userName, userId, message);
+
+    const payload = {
+        username: userName,
+        id: userId,
+        message
+    }
+    axios.get('/api/', payload)
+    .then(response => {
+        console.log(response)
+    })
+}
+
+function getAllUsers() {
+    axios.get('/getallusers/')
+    .then(response => {
+        document.getElementById('result').innerHTML = JSON.stringify(response.data)
+    })
+}
--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.use('/', express.static('public'));
 
 app.get('/', (req, res) => res.send('Hello World'));
 
-app.get('api', function(req, res) {
-    const userName = req.query.username;
-    const userId = req.query.id;
-    const message = req.query.message;
+var data = [];
+app.post('api', function (req, res) {
+    const userName = req.body.username;
+    const userId = req.body.id;
+    const message = req.body.message;
     console.log(userName);
+
+    const temp = {
+        username: userName,
+        id: userId,
+        message: message
+    }
+
+    data.push(temp)
+    console.log(data);
     const reply = `${userName} with id of ${userId} is saying ${message}`
     res.send(reply);
+})
+
+app.get('/getallusers', function (req, res) {
+    res.send(data)
 })
 
 app.get('/showprofile/:username', function (req, res) {
@@ -21,7 +38,6 @@ app.get('/showprofile/:username', function (req, res) {
 
     res.send('show profile working');
 })
-
 
 
 app.listen(port, () => console.log(`Tommie's Bargain, now listening on port ${port}!`));
