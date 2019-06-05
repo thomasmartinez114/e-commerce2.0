4fdeb615a691283a6a0c1163fabe54f66ccdf291
unknown <tmartinez114@gmail.com>
2019-06-05 13:44:52 -0400

    made users go into table and added color for single user search

--- userApp/public/script.js
+++ userApp/public/script.js
@@ -1,34 +1,71 @@
-function getUserInfo() {
-  const input = document.getElementById("userName").value;
+const getUserInfo = () => {
+  const input = document.querySelector("#userName").value;
   const url = "/api/showprofile/" + input;
-  axios.get(url)
-    .then(response => {
-      document.getElementById("userInfo").innerHTML = JSON.stringify(response.data);
-    })
-}
 
-function handleSubmit() {
-  // Getting values from user inputs
-  const userName = document.getElementById("user-name").value;
+  axios.get(url).then(response => {
+    const listItems = response.data.map(element => {
+      return (
+        "<li class='get-all-users'>" +
+        "Name: " +
+        (element.username
+          ? element.username
+          : " " + element.username + " was not found.") +
+        " | " +
+        "message: " +
+        (element.message
+          ? element.message
+          : " " + element.username + " did not leave a message.") +
+        "</li>"
+      );
+    });
+    document.querySelector("#userInfo").innerHTML =
+      "<ul>" + listItems.join("\n") + "</ul>";
+    console.log(response);
+  });
+};
 
-  const message = document.getElementById("message").value;
+function displayUsers(userData, id) {}
 
-  // Creating a payload object
-  const payload = {
-    username: userName,
-    message
-  }
+const handleSubmit = () => {
+  const username = document.querySelector("#user-Name").value;
+  const message = document.querySelector("#userMsg").value;
 
-  // Making a post request with axios
-  axios.post("/api/", payload)
-    .then(response => {
-      console.log(response.data)
-    })
-}
+  const url2 = "api/";
 
-function getAllUsers() {
-  axios.get("/api/getallusers/")
-    .then(response => {
-      document.getElementById("result").innerHTML = JSON.stringify(response.data)
-    })
-}
+  axios.post(url2, { username, message }).then(response => {
+    // console.log(response);
+    document.querySelector("#submitInfo").innerHTML = response.data;
+  });
+};
+
+const getAllUsers = () => {
+  const url3 = "/api/getallusers";
+  axios.get(url3).then(response => {
+    const listItems = response.data.map(item => {
+      return (
+        "<tr>" +
+        "<td>" +
+        item.username +
+        "</td>" +
+        "<td>" +
+        (item.message
+          ? item.message
+          : " " + item.username + " did not leave a message") +
+        "</td>" +
+        "</tr>"
+      );
+    });
+    document.querySelector("#result").innerHTML =
+      "<table style='width:100%'>" +
+      "<tr>" +
+      "<th>" +
+      "Username" +
+      "</th>" +
+      "<th>" +
+      "Message" +
+      "</th>" +
+      "</tr>" +
+      listItems.join(" ") +
+      "</table>";
+  });
+};
