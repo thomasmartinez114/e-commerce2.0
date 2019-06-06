d507c2e07d46f3a7db170b3c992914d577159247
unknown <tmartinez114@gmail.com>
2019-06-06 16:47:21 -0400

    added fields for insert

--- tommiesBargains/server.js
+++ tommiesBargains/server.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use('/', express.static('public'));
+
 app.get("/api", function(req, res) {
   const sellerName = req.query.sellername;
   const sellerCity = req.query.sellercity;
   const sellerState = req.query.sellerstate;
   const productName = req.query.productname;
   const productPrice = req.query.productprice;
-  const description = req.query.description;
+  const description = req.query.productdescription;
 
   console.log(sellerName, sellerCity, sellerState, productName, productPrice, description);
   const reply = `${sellerName} from ${sellerCity}, ${sellerState} is selling a ${productName} for $${productPrice}`
