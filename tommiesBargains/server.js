33d442cd864003405d0f301d9f826c7eb6b0aab4
unknown <tmartinez114@gmail.com>
2019-06-05 20:52:20 -0400

    installed nodemon

--- tommiesBargains/server.js
+++ tommiesBargains/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => res.send("Hello World"));
+
 app.listen(port, () =>
   console.log(`Tommie's Bargain App is running on port: ${port}`)
 );
