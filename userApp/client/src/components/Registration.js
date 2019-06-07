88f8b6e772304c2a34ff3f4a5ee373378cb2e144
unknown <tmartinez114@gmail.com>
2019-06-06 21:34:57 -0400

    added this.state | handleClick method

--- userApp/client/src/components/Registration.js
+++ userApp/client/src/components/Registration.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
 class Registration extends Component{
+    state={
+        userName: '',
+        message: ''
+    }
+    
     render(){
         return (
             <div>
-                <input placeholder="User Name" />
-                <input placeholder="Message" />
+                <h3>User Registration</h3>
+                <input
+                  onChange={ e => this.setState({userName: e.target.value})}
+                  placeholder="User Name"
+                />
+                <input
+                  onChange={ e => this.setState({message: e.target.value})}
+                  placeholder="Message"
+                />
                 <button>Submit</button>
+                {this.state.userName}
+                {this.state.message}
             </div>
         )
     }
