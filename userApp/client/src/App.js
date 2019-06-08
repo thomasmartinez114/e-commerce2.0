42ec46fc6bb52b6cdc1731b9f901789e495472c6
unknown <tmartinez114@gmail.com>
2019-06-08 13:56:44 -0400

    push 6/8/2019 class

--- userApp/client/src/App.js
+++ userApp/client/src/App.js
@@ -1,19 +1,44 @@
-import React, { Component } from 'react'
-import Registration from './components/Registration';
-import UserInfo from './components/UserInfo';
-import AllUsers from './components/AllUsers';
+import React, { Component } from "react";
+import Registration from "./components/Registration";
+import UserInfo from "./components/UserInfo";
+import AllUsers from "./components/AllUsers";
 
 export default class App extends Component {
+  state = {
+    pageView: ""
+  };
+
   render() {
     return (
-      <div>
+      <div style={{ margin: "10px", textAlign: "center" }} className="App">
         <h2>My Express App</h2>
-        <Registration />
-        <h3>Get User Info</h3>
-        <UserInfo />
-        <h4>Get All Users</h4>
-        <AllUsers />
+        <a
+          style={{ margin: "10px" }}
+          href="#"
+          onClick={() => this.setState({ pageView: <Registration /> })}
+        >
+          Register
+        </a>
+
+        <a
+          style={{ margin: "10px" }}
+          href="#"
+          onClick={() => this.setState({ pageView: <UserInfo /> })}
+        >
+          User Info
+        </a>
+
+        <a
+          style={{ margin: "10px" }}
+          href="#"
+          onClick={() => this.setState({ pageView: <AllUsers /> })}
+        >
+          All Users
+        </a>
+
+        <br />
+        {this.state.pageView}
       </div>
-    )
+    );
   }
 }
--- userApp/client/src/components/Registration.js
+++ userApp/client/src/components/Registration.js
@@ -1,34 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-class Registration extends Component{
-    state={
-        userName: '',
-        message: ''
-    }
-    
-    handleClick = () => {
-        console.log('userName:', this.state.userName);
-        console.log('message:', this.state.message);
-    }
-    
-    render(){
-        return (
-            <div>
-                <h3>User Registration</h3>
-                <input
-                  onChange={ e => this.setState({userName: e.target.value})}
-                  placeholder="User Name"
-                />
-                <input
-                  onChange={ e => this.setState({message: e.target.value})}
-                  placeholder="Message"
-                />
-                <button onClick={this.handleClick}>Submit</button>
-                {this.state.userName}
-                {this.state.message}
-            </div>
-        )
-    }
+class Registration extends Component {
+  state = {
+    userName: "",
+    message: ""
+  };
+
+  handleClick = () => {
+    console.log("userName:", this.state.userName);
+    console.log("message:", this.state.message);
+
+    const data = {
+      username: this.state.userName,
+      message: this.state.message
+    };
+
+    axios.post("/api", data)
+    .then(res => console.log(res.data))
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>User Registration</h3>
+        <input
+          onChange={e => this.setState({ userName: e.target.value })}
+          placeholder="User Name"
+        />
+        <input
+          onChange={e => this.setState({ message: e.target.value })}
+          placeholder="Message"
+        />
+        <button onClick={this.handleClick}>Submit</button>
+        {this.state.userName}
+        {this.state.message}
+      </div>
+    );
+  }
 }
 
-export default Registration
+export default Registration;
