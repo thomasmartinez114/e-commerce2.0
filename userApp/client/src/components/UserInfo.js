56fbcf3c00584e56fc40b96a54999433644215ab
unknown <tmartinez114@gmail.com>
2019-06-11 09:39:17 -0400

    added

--- userApp/client/src/components/UserInfo.js
+++ userApp/client/src/components/UserInfo.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class UserInfo extends Component {
-    state={
-        userName: ''
-    }
+  state = {
+    userName: "",
+    result: ""
+  };
 
-    handleClick = () => {
-        console.log('userName', this.state.userName);
-    }
-    render(){
-        return(
-            <div>
-                <input
-                  onChange={ e => this.setState({userName: e.target.value})}
-                  placeholder='User Name'
-                />
-                <button onClick={this.handleClick}>Get User Info</button>
-            </div>
-        )
-    }
+  handleClick = () => {
+    console.log("state", this.state);
+    console.log("props", this.props);
+    this.setState({
+      result: this.props.data
+    });
+  };
+  render() {
+    return (
+      <div>
+        <input
+          onChange={e => this.setState({ userName: e.target.value })}
+          placeholder="User Name"
+        />
+        <button onClick={this.handleClick}>Get User Info</button>
+        <br />
+        {this.state.result}
+      </div>
+    );
+  }
 }
 
-export default UserInfo
+export default UserInfo;
