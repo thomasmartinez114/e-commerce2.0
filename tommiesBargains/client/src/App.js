9cfed99a2bf7e98822eddf69f62c1bd2cdd3ef09
unknown <tmartinez114@gmail.com>
2019-06-12 23:51:33 -0400

    added state to react

--- tommiesBargains/client/src/App.js
+++ tommiesBargains/client/src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 
-function App() {
-  return <div className="App">      </div>;
+class App extends Component {
+  state = {
+    userName: ""
+  };
+
+  getSellerInfo = () => {
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Welcome to Tommie's Bargains!</h2>
+        <input
+          onChange={e =>
+            this.setState({
+              userName: e.target.value
+            })
+          }
+          placeholder="user name"
+          type="text"
+        />
+        <button onClick={this.getSellerInfo}>Get seller info</button>      
+      </div>
+    );
+  }
 }
 
 export default App;
