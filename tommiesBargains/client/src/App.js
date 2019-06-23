16f20e284be9d48bf91fdcb96e4ff1ac72b7ccd7
unknown <tmartinez114@gmail.com>
2019-06-23 12:30:04 -0400

    installed concurrently but removed it from app

--- tommiesBargains/client/src/App.js
+++ tommiesBargains/client/src/App.js
@@ -4,11 +4,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-               <h2>My Express App</h2>
-               
+        <h2>My Express App</h2>     
         <input placeholder="user name" type="text" />
-               <button>Get user info</button>
-             
+        <button>Get user info</button>    
       </div>
     );
   }
